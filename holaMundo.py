5302af071ed3dcaf25f31b077723e5185e3e1915
RoyZamudio <royzamudiotovar@gmail.com>
2023-11-21 09:17:03 -0600

    Practica 1

---
+++ holaMundo.py
@@ -0,0 +1,7 @@
+def saludar(nombre, matricula):
+    print(f"Hola Mundo, {Rodrigo Zamudio Tovar}!")
+    print(f"Tu número de matrícula es: {2213026292}")
+
+if __name__ == "__main__":
+
+    saludar(nombre, matricula)
